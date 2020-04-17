456385b4804c40762e47b0b066608e362bce976a
thedevyansh <schawla333333@gmail.com>
2020-04-17 18:52:55 +0530

    added code to collect api data in arrays

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,49 @@
 <script>
 
 import axios from "axios";
+import moment from "moment";
 
 export default {
   name: 'App',
   components: {
 
   },
+  data() {
+    return {
+
+      arrPositive: [],
+      arrHospitalized: [],
+      arrInIcu: [],
+      arrOnVentilators: [],
+      arrRecovered: [],
+      arrDeaths: []
+    };
+  },
   async created() {
     const { data } = await axios.get("https://covidtracking.com/api/us/daily");
-    console.log(data);
+    
+    data.forEach(d => {
+
+      const date = moment(d.date, "YYYYMMDD").format("MM/DD");
+      const {
+        positive,
+        hospitalizedCurrently, 
+        inIcuCurrently,
+        onVentilatorCurrently,
+        recovered,
+        death
+      } = d;
+
+      this.arrPositive.push({ date, total: positive });
+      this.arrHospitalized.push({ date, total: hospitalizedCurrently });
+      this.arrInIcu.push({ date, total: inIcuCurrently });
+      this.arrOnVentilators.push({ date, total: onVentilatorCurrently });
+      this.arrRecovered.push({ date, total: recovered });
+      this.arrDeaths.push({ date, total: death });
+      
+    })
   }
-}
+};
 
 </script>
 
