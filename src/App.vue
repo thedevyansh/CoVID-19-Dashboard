9b1a459dc78fffc03cdf24a7e1b049e4c6ce7059
thedevyansh <schawla333333@gmail.com>
2020-04-17 18:09:08 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  async created() {
+    const { data } = await axios.get("https://covidtracking.com/api/us/daily");
+    console.log(data);
+  }
+}
+
+</script>
+
+<style>
+</style>
